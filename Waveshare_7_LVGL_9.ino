2b76e3b11fc27e2ba6eabb8b04b70b8a5e801574
Paul Hamshere <p.w.hamshere@gmail.com>
2024-07-31 08:46:12 +0100

    Add files via upload
    Fix touch.h so that it detects address of the GT911, as in this version we don't reset it with a known level of GPIO 4 (INT).

---
+++ Waveshare_7_LVGL_9.ino
@@ -0,0 +1,182 @@
+#include <lvgl.h>
+#include <demos/lv_demos.h>
+#include <Arduino_GFX_Library.h>
+#include <Arduino.h>
+
+// Extend IO Pin define
+
+/*
+#define TP_RST   (1)
+#define LCD_BL   (2)
+#define LCD_RST  (3)
+#define SD_CS    (4)
+#define USB_SEL  (5)
+*/
+
+#define TP_RST   (1ULL << 1)
+#define LCD_BL   (1ULL << 2)
+#define LCD_RST  (1ULL << 3)
+#define SD_CS    (1ULL << 4)
+#define USB_SEL  (1ULL << 5)
+
+/*
+#define TP_RST   (1ULL << 0)
+#define LCD_BL   (1ULL << 1)
+#define LCD_RST  (1ULL << 2)
+#define SD_CS    (1ULL << 3)
+#define USB_SEL  (1ULL << 4)
+*/
+
+#include <ESP_IOExpander_Library.h>
+#include "Wire.h"
+
+#define I2C_MASTER_NUM              I2C_NUM_0
+#define I2C_MASTER_SDA_IO   8
+#define I2C_MASTER_SCL_IO   9
+#define GPIO_INPUT_IO_4     4
+//#define GPIO_INPUT_PIN_SEL  1ULL << GPIO_INPUT_IO_4
+//#define ESP_IO_EXPANDER_I2C_CH422G_ADDRESS 0x24
+//#define ESP_PANEL_EXPANDER_I2C_ADDRESS          (0x24)
+
+// redefine this in lv_code.ino
+void lv_my_setup();
+
+#define GFX_DEV_DEVICE WAVESHARE_ESP32_S3_TFT_7
+#define GFX_BL (-1)
+
+Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
+     5 /* DE */,  3 /* VSYNC */, 46 /* HSYNC */, 7 /* PCLK */,
+     1 /* R0 */,  2 /* R1 */,    42 /* R2 */,    41 /* R3 */, 40 /* R4 */,     
+     39 /* G0 */, 0 /* G1 */,    45 /* G2 */,    48 /* G3 */, 47 /* G4 */, 21 /* G5 */,
+     14 /* B0 */, 38 /* B1 */,   18 /* B2 */,    17 /* B3 */, 10 /* B4 */,
+     0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 8 /* hsync_back_porch */,
+     0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 8 /* vsync_back_porch */,
+     1 /* pclk_active_neg */, 16000000 /* prefer_speed */);
+ 
+ Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
+     800 /* width */, 480 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */);
+
+/*******************************************************************************
+ * Please config the touch panel in touch.h
+ ******************************************************************************/
+#include "touch.h"
+
+static uint32_t my_tick_function(void) {
+  return millis();
+}
+
+static const uint32_t screenWidth = 800;
+static const uint32_t screenHeight = 480;
+//int buf_size_in_bytes;
+static const int buf_size_in_bytes = screenWidth * screenHeight * sizeof(lv_color_t) / 10;
+static lv_color_t *disp_draw_buf, *disp_draw_buf2;
+// Use below if not dynamically allocating memory
+//static uint16_t disp_draw_buf[buf_size_in_bytes / sizeof(lv_color_t)];
+
+// Display flushing 
+void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)px_map, w, h);
+  lv_disp_flush_ready(disp);
+}
+
+void my_touchpad_read(lv_indev_t *indev_driver, lv_indev_data_t *data)
+{
+  if (touch_touched()) {
+    data->state = LV_INDEV_STATE_PR;
+
+    //Set the coordinates
+    data->point.x = touch_last_x;
+    data->point.y = touch_last_y;
+  }
+  else 
+    data->state = LV_INDEV_STATE_REL;
+}
+
+lv_display_t *disp;
+lv_indev_t   *indev;
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(GPIO_INPUT_IO_4, OUTPUT);
+
+  /*
+  //ESP_IOExpander *expander = new ESP_IOExpander_CH422G((i2c_port_t)I2C_MASTER_NUM, ESP_IO_EXPANDER_I2C_CH422G_ADDRESS_000, I2C_MASTER_SCL_IO, I2C_MASTER_SDA_IO);
+  //ESP_IOExpander *expander = new ESP_IOExpander_CH422G((i2c_port_t)I2C_MASTER_NUM, ESP_IO_EXPANDER_I2C_CH422G_ADDRESS_000);
+  ESP_IOExpander *expander = new ESP_IOExpander_CH422G((i2c_port_t)I2C_MASTER_NUM, ESP_IO_EXPANDER_I2C_CH422G_ADDRESS_000, 9, 8);
+  
+  expander->init();
+  expander->begin();
+  expander->multiPinMode(TP_RST | LCD_BL | LCD_RST | SD_CS | USB_SEL, OUTPUT);
+  expander->multiDigitalWrite(TP_RST | LCD_BL | LCD_RST, HIGH);
+  delay(100);  
+  expander->multiDigitalWrite(TP_RST | LCD_RST, LOW);
+  delay(100);
+  digitalWrite(GPIO_INPUT_IO_4, LOW);
+  delay(100);
+  expander->multiDigitalWrite(TP_RST | LCD_RST, HIGH);
+  delay(200);
+  */
+
+
+  gfx->begin();
+  gfx->fillScreen(RED);
+
+#ifdef GFX_BL
+  pinMode(GFX_BL, OUTPUT);
+  digitalWrite(GFX_BL, HIGH);
+#endif
+
+  touch_init();
+  lv_init();
+
+#ifdef ESP32
+  Serial.println("ESP32 capability draw buffer malloc()");
+  disp_draw_buf = (lv_color_t *) heap_caps_malloc(buf_size_in_bytes, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+  disp_draw_buf2 = (lv_color_t *) heap_caps_malloc(buf_size_in_bytes, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+#else
+  Serial.println("Normal draw buffer malloc()");
+  disp_draw_buf = (lv_color_t *) malloc(buf_size_in_bytes);
+  disp_draw_buf2 = (lv_color_t *) malloc(buf_size_in_bytes);
+#endif
+
+  if (disp_draw_buf == nullptr)
+  {
+    Serial.println("LVGL disp_draw_buf allocate failed!");
+    while (1);
+  }
+
+  if (disp_draw_buf2 == nullptr)
+  {
+    Serial.println("LVGL disp_draw_buf2 allocate failed - carry on anyway we can live without it");
+  }
+
+
+  // set up LVGL
+  lv_tick_set_cb(my_tick_function);
+
+  disp = lv_display_create(screenWidth, screenHeight);
+  lv_display_set_flush_cb(disp, my_disp_flush);
+  lv_display_set_buffers(disp, disp_draw_buf, disp_draw_buf2, buf_size_in_bytes, LV_DISPLAY_RENDER_MODE_PARTIAL);
+
+  indev = lv_indev_create();
+  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+  lv_indev_set_read_cb(indev, my_touchpad_read);
+
+  //lv_demo_widgets();
+
+  // do my setup
+  lv_my_setup();
+
+  Serial.println("Setup complete");
+}
+
+void loop()
+{
+  lv_timer_handler(); 
+  delay(5);
+}
---
+++ touch.h
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Touch libraries:
+  * GT911: https://github.com/TAMCTec/gt911-arduino.git
+ ******************************************************************************/
+
+/* uncomment for GT911 */
+#define TOUCH_GT911
+#define TOUCH_GT911_SCL 9
+#define TOUCH_GT911_SDA 8
+#define TOUCH_GT911_INT 4
+#define TOUCH_GT911_RST -1
+#define TOUCH_GT911_ROTATION ROTATION_NORMAL // ROTATION_LEFT
+#define TOUCH_MAP_X1 800
+#define TOUCH_MAP_X2 0
+#define TOUCH_MAP_Y1 480
+#define TOUCH_MAP_Y2 0
+
+int touch_last_x = 0, touch_last_y = 0;
+
+#include <Wire.h>
+#include <TAMC_GT911.h>
+TAMC_GT911 ts = TAMC_GT911(TOUCH_GT911_SDA, TOUCH_GT911_SCL, TOUCH_GT911_INT, TOUCH_GT911_RST, max(TOUCH_MAP_X1, TOUCH_MAP_X2), max(TOUCH_MAP_Y1, TOUCH_MAP_Y2));
+
+void touch_init()
+{
+
+  // Find the GT911 at either 0x5D or 0x14 - given we aren't using the IO expander to reset the GT911
+
+  Wire.begin(TOUCH_GT911_SDA, TOUCH_GT911_SCL);
+
+  byte error;
+  bool done;
+  done = false;
+  int gt_addr[]{GT911_ADDR1, GT911_ADDR2};
+  int addr;
+
+  for (int i = 0; (i < 2) && !done; i++) {
+    int addr = gt_addr[i];  
+    Wire.beginTransmission(addr);
+    error =  Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("GT911 found at 0x");
+      Serial.println(addr, HEX);
+      ts.begin(addr);
+      done = true;
+    }
+  }
+
+  if (!done) {
+    Serial.println("NO GT911 FOUND!");
+  }
+
+  //ts.begin();
+  ts.setRotation(TOUCH_GT911_ROTATION);
+}
+
+bool touch_touched()
+{
+  ts.read();
+  if (ts.isTouched) {
+#if defined(TOUCH_SWAP_XY)
+    touch_last_x = map(ts.points[0].y, TOUCH_MAP_X1, TOUCH_MAP_X2, 0, gfx->width() - 1);
+    touch_last_y = map(ts.points[0].x, TOUCH_MAP_Y1, TOUCH_MAP_Y2, 0, gfx->height() - 1);
+#else
+    touch_last_x = map(ts.points[0].x, TOUCH_MAP_X1, TOUCH_MAP_X2, 0, gfx->width() - 1);
+    touch_last_y = map(ts.points[0].y, TOUCH_MAP_Y1, TOUCH_MAP_Y2, 0, gfx->height() - 1);
+#endif
+    return true;
+  }
+  else {
+    return false;
+  }
+}
